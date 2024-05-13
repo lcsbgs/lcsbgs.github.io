70e8027e225b660e117a2ef1928e9f2117c508a8
Lucas Borges <lucasb.borges@icloud.com>
2024-05-13 00:54:47 -0300

    password

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function checkPassword() {
     var password = document.getElementById('passwordInput').value;
-    if (password === 'bolo') {
+    if (password === '01100010 01101111 01101100 01101111') {
         window.location.href = 'pagina2.html';
     } else {
         alert('Senha incorreta. Tente novamente.');
